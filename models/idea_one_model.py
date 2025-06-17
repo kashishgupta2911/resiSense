05535f33d8c2dbd7a63e2d3a9649db482295f33b
mylayambao <91172629+mylayambao@users.noreply.github.com>
2025-06-17 09:21:14 -0600

    group rent

--- models/idea_one_model.py
+++ models/idea_one_model.py
@@ -151,8 +151,8 @@ bound_bike_scores = list(bounds_dict['bikescore'][user_input['bikescore']])
 
 #%%
 # make a "hypothetical" dataframe with the combinations of the bounds lists
-# the order for each row should be bound_affordability2019, bound_affordability2020, bound_affordability2021, bound_affordability2022, bound_affordability ,bound_crime_occurances, bound_walk_scores, bound_transit_scores, bound_bike_scores
-# Group the historical values
+
+# group the historical values
 historical_affordability_bounds = list(zip(
     bound_affordability2019,
     bound_affordability2020,
@@ -189,8 +189,6 @@ columns = [
 ]
 combo_df = combo_df[columns]
 print(combo_df)
-
-
 
 
 
@@ -267,6 +265,7 @@ print(f'Mean Absolute Error: {mae:.4f}')
 #%%
 # TEST THE MODEL USING THE NEW COMBO DATAFRAME
 y_pred_using_combo = model.predict(combo_df)
+
 
 # print the columns and the predictions
 for i in range(len(y_pred_using_combo)):
